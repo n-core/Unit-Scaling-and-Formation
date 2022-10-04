311a23f9d5ce87528919b37a933a352f344c40de
N.Core <ncore.station@gmail.com>
2022-10-04 17:35:44 +0700

    fix pikeman size typo

--- SQL/USnF-1-SCALE/Base Game/USnF-BASE_LAND.sql
+++ SQL/USnF-1-SCALE/Base Game/USnF-BASE_LAND.sql
@@ -142,7 +142,7 @@ INSERT  INTO ArtDefine_USnF (USnF_UnitInfo, USnF_UnitMemberInfo, USnF_Type, USnF
             (   'ART_DEF_UNIT_LONGSWORDSMAN',       'ART_DEF_UNIT_MEMBER_LONGSWORDSMAN',        'RUG',  'LAND', 0.18,   3,          'DefaultMeleeRUG'),
             (   'ART_DEF_UNIT_LONGSWORDSMAN',       'ART_DEF_UNIT_MEMBER_LONGSWORDSMAN',        'SUG',  'LAND', 0.25,   1,          ''),
         --	PIKEMAN
-            (   'ART_DEF_UNIT_PIKEMAN',             'ART_DEF_UNIT_MEMBER_PIKEMAN',              'RED',  'LAND', 0.19,   16,         'PikemanRED'),
+            (   'ART_DEF_UNIT_PIKEMAN',             'ART_DEF_UNIT_MEMBER_PIKEMAN',              'RED',  'LAND', 0.09,   16,         'PikemanRED'),
             (   'ART_DEF_UNIT_PIKEMAN',             'ART_DEF_UNIT_MEMBER_PIKEMAN',              'RUG',  'LAND', 0.18,   3,          'PikemanRUG'),
             (   'ART_DEF_UNIT_PIKEMAN',             'ART_DEF_UNIT_MEMBER_PIKEMAN',              'SUG',  'LAND', 0.25,   1,          ''),
         --	TREBUCHET
